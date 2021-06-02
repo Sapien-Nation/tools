e43bfe2f1b46c74316a66cb7009faeaa1eb3e233
mtabasco <matabasco@gmail.com>
2021-06-02 13:08:35 +0200

    make changes requested

--- types/content.ts
+++ types/content.ts
@@ -1,21 +1,33 @@
-export interface Content {
+type contentType = 'POST' | 'REPLY';
+type contentGroupType = 'SQUARE' | 'CHANNEL';
+
+export interface ContentOnwer {
+  avatar: string,
+  displayName: string,
+  id: string,
+  userName: string,
+}
+
+export interface ContentTribe {
+  name: string;
   id: string;
-  data: string;
-  createdAt: string;
-  type: string;
-  contentGroupType?: string;
-  contentGroupName?: string;
-  owner: {
-    id: string,
-    displayName: string,
-  }
 }
 
-export interface Post extends Content {
-  type: 'POST'
+export interface ContentGroup { // Groups can be Channels or Squares
+  id: string;
+  name: string;
+  type: contentGroupType;
 }
 
-export interface Reply extends Content {
-  type: 'REPLY',
-  postId: string,
+export interface Content {
+  group: ContentGroup;
+  createdAt: ISOString;
+  data: RAWHtml;
+  id: string;
+  image?: string; // optional image, maybe not needed on replies preview
+  owner: ContentOnwer;
+  postId?: string, // only for replies, the postId linked to the reply
+  topics?: Array<string>;
+  tribe: ContentTribe;
+  type: contentType; // post / reply
 }
