93d741c5aa51c885c7eea2d6d17f31ad0ec63207
Katya de la Rosa <katyadelarosa@gmail.com>
2021-06-16 18:42:31 -0500

    TRB: 000 Square feed

--- types/square/view.ts
+++ types/square/view.ts
@@ -1,7 +1,12 @@
-// TODO remove
+import { Content } from '../content';
 export interface Square {
   description: string;
   id: string;
   membersCount: number;
   name: string;
+}
+
+export interface SquareFeed {
+  data: Array<Content>;
+  nextCursor: string | null;
 }
