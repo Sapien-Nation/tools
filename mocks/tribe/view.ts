ff13386f3c83078072790a6ce660b3066b45082f
Katya de la Rosa <katyadelarosa@gmail.com>
2021-06-22 21:44:48 -0500

    Fix tribe view mock

--- mocks/tribe/view.ts
+++ mocks/tribe/view.ts
@@ -2,7 +2,9 @@ import type { Tribe } from '../../types/tribe/view';
 
 export const mockTribeMedia = (props: Partial<Tribe> = {}): Tribe => ({
   avatar: '/fixtures/256x256/message.png',
+  avatar_original: '/fixtures/256x256/message.png',
   cover: '/fixtures/normal/1920x200.png',
+  cover_original: '/fixtures/normal/1920x200.png',
   description: '',
   followersCount: 0,
   identifier: 'sapien',
