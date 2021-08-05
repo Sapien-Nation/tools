0650af2e5e127373eb948ba6fb07e40b90de25cc
mtabasco <matabasco@gmail.com>
2021-08-05 06:55:24 +0200

    changes on badge type

--- types/badge.ts
+++ types/badge.ts
@@ -1,8 +1,9 @@
 export interface Badge {
   id: string, // id of the Badge on Sapien
-  blockchainId: number, // id of the Badge on the blockchain
+  blockchainId?: number, // id of the Badge on the blockchain
   name: string, 
-  description: string,
+  description?: string,
   avatar: string, 
   spn: number, // Badge price, no decimals
+  quantity?: number // number of badges of the same type
 }
