cb9aec35a33eb5eb0ba89abaad326bb9c0f214e6
javier ocanas <javierocanas@gmail.com>
2021-08-05 15:17:49 -0500

    badge type update

--- types/badge.ts
+++
@@ -1,9 +0,0 @@
-export interface Badge {
-  id: string, // id of the Badge on Sapien
-  blockchainId?: number, // id of the Badge on the blockchain
-  name: string, 
-  description?: string,
-  avatar: string, 
-  spn: number, // Badge price, no decimals
-  quantity?: number // number of badges of the same type
-}
--- types/wallet/badge.ts
+++ types/wallet/badge.ts
@@ -1,5 +1,9 @@
 export interface Badge {
-  price: number;
+  id: string; // id of the Badge on Sapien
+  blockchainId?: number; // id of the Badge on the blockchain
   name: string;
-  description: string;
+  description?: string;
+  avatar: string;
+  spn: number; // Badge price, no decimals
+  quantity?: number; // number of badges of the same type
 }
