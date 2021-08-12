93affef65edf6e106e0f8085c5527055f76d09db
Ethaan <ethan.rosanoo@gmail.com>
2021-08-12 12:11:26 -0500

    Fix for Channel tools

--- mocks/channel/view.ts
+++ mocks/channel/view.ts
@@ -3,7 +3,9 @@ import type { Channel, ChannelFeed } from '../../types/channel/view';
 
 export const mockChannel = (props: Partial<Channel> = {}): Channel => ({
   avatar: '/fixtures/256x256/message.png',
+  avatar_original: '/fixtures/256x256/message.png',
   cover: '/fixtures/normal/1920x200.png',
+  cover_original: '/fixtures/normal/1920x200.png',
   description: 'Channel Description',
   name: 'Channel Name',
   membersCount: 0,
