1f15615fda4c767b4151f8ee454a835456d8c880
Katya de la Rosa <katyadelarosa@gmail.com>
2021-08-26 14:45:55 -0500

    Adding member badge to tribe

--- mocks/tribe/view.ts
+++ mocks/tribe/view.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import type { Tribe } from '../../types/tribe/view';
 
 export const mockTribeMedia = (props: Partial<Tribe> = {}): Tribe => ({
@@ -8,7 +10,17 @@ export const mockTribeMedia = (props: Partial<Tribe> = {}): Tribe => ({
   description: '',
   followersCount: 0,
   identifier: 'sapien',
+  isMember: false,
   membersCount: 0,
+  memberBadge: {
+    avatar: '/fixtures/256x256/message.png',
+    id: uuidv4(),
+    price: 0,
+    blockchainId: uuidv4(),
+    name: 'Sapien badge',
+    parentBadgeId: uuidv4(),
+  },
   name: 'Sapien',
+  role: null,
   ...props,
 });
