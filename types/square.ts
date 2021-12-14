e7d1a750a8e02b77ea66734cc316b64c98553322
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2021-12-13 22:51:02 -0600

    feat: Adding MainSquare type

--- types/square.ts
+++ types/square.ts
@@ -1,4 +1,22 @@
+// types
+import { Media } from './common';
+
 export interface Square {
+  description: string;
   id: string;
+  membersCount: number;
   name: string;
+}
+
+export interface MainSquare {
+  avatar: Media;
+  avatar_original: Media;
+  cover: Media;
+  cover_original: Media;
+  description: string;
+  followersCount: number;
+  id: string;
+  identifier: string;
+  name: string;
+  membersCount: number;
 }
