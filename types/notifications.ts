3cce5e25c9682f99dffc92c46cc0b8a5cffdf158
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2021-12-19 17:31:02 -0600

    feat: update NotificationType enum

--- types/notifications.ts
+++ types/notifications.ts
@@ -1,9 +1,23 @@
 // types
 import type { ISOString } from './common';
 
-// TODO more replies type
 export enum NotificationType {
+  BadgeUser = 'token.badge_user',
+  BadgePost = 'token.badge_post',
+  BadgePurchased = 'store.badge_purchased',
+  BadgePurchasedForAdmin = 'store.badge_purchased_for_admin',
+  BadgePriceUpdatedForAdmin = 'store.badge_price_updated_for_admin',
+  ChannelInvite = 'tribe.channel_invite',
+  DepositSPN = 'token.deposit_spn',
+  JoinRequestResolved = 'tribe.join_request_resolved',
+  LeaveTribe = 'store.leave_tribe',
+  Mentioned = 'tribe.mentioned',
   NewReply = 'tribe.new_reply',
+  ReportPost = 'tribe.report_post',
+  SendSPNUser = 'token.send_spn_user',
+  SendSPNPost = 'token.send_spn_post',
+  SapienBadgeReceived = 'store.sapien_badge_received',
+  WithdrawSPN = 'token.withdraw_spn',
 }
 
 export interface NotificationReceiver {
