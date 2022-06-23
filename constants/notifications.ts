640b70182daaa5be23aecf206dac1986faf37961
carlos <enlozanor@gmail.com>
2022-06-23 14:31:11 -0700

    feat: update notifications type

--- constants/notifications.ts
+++ constants/notifications.ts
@@ -2,5 +2,6 @@ export enum NotificationsType {
   BadgeGrant = 'badge.grant-badge',
   BadgeGrantOwner = 'badge.grant-owner-badge',
   BadgeGrantPropose = 'badge.propose-grant-badge',
+  BadgeGrandProposeReady = 'badge.propose-grant-badge-ready',
   RoomNewMessage = 'room.new-message',
 }
