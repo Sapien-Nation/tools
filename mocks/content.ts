8cd56ec0c0e7c4e65dfd45521f8b0befb6b103c6
carlos <enlozanor@gmail.com>
2022-08-25 10:27:18 -0700

    feat: update content mock

--- mocks/content.ts
+++ mocks/content.ts
@@ -35,9 +35,11 @@ export const mockContent = ({
   createdAt: new Date().toISOString(),
   type: ContentType.POST,
   owner: mockContentOwner(),
-  imagePreview: null,
+  preview: null,
   mimeType: ContentMimeType.Html,
   threads: [],
   title: 'Lorem Ipsum',
+  media: '',
+  link: '',
   ...rest,
 });
